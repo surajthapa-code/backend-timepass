505c506cb53b5321ddc4716a38a3568038a1c23c
Suraj Thapa <surajthapa7787@gmail.com>
2026-06-30 09:52:45 +0530

    learned--> post req, get req , middle wares ,next(),

---
+++ post req.js
@@ -0,0 +1,23 @@
+import express from "express";
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(` incoming ${req.method} request at ${req.url} `);
+  next();
+});
+app.post("/articles", (req, res) => {
+  const { title, content, author } = req.body;
+  //save in the data base here
+
+  res.status(201).json({
+    tittle,
+    content,
+    author,
+  });
+});
+
+app.listen(3000, () => {
+  console.log(`service live at http://localhost:3000/articles`);
+});
