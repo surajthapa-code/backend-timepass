32ba938277168fc3cf895559891b61d10d73a91d
Suraj Thapa <surajthapa7787@gmail.com>
2026-06-30 11:47:44 +0530

    how to use zod

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-//jod 
+//jod
+import express from "express";
+import { z } from "zod";
+
+const userSchema = z.object({
+  username: z.string().min(3),
+  email: z.string().email(),
+  age: z.number().min(18),
+});
+const app = express();
