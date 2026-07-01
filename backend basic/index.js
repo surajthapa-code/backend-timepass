52502a34f705c1fd0ca918337c3b9c2a6a81b465
Suraj Thapa <surajthapa7787@gmail.com>
2026-07-01 07:33:00 +0530

    learned file structure

--- backend basic/index.js
+++ backend basic/index.js
@@ -1,3 +1,4 @@
+//jod
 import express from "express";
 const app = express();
 
@@ -6,7 +7,7 @@ app.use(express.json());
 app.use((req, res, next) => {
   console.log(` incoming ${req.method} request at ${req.url} `);
   next();
-});
+}); 
 app.post("/articles", (req, res) => {
   const { title, content, author } = req.body;
   //save in the data base here
@@ -21,3 +22,13 @@ app.post("/articles", (req, res) => {
 app.listen(3000, () => {
   console.log(`service live at http://localhost:3000/articles`);
 });
+
+import express from "express";
+import { z } from "zod";
+
+const userSchema = z.object({
+  username: z.string().min(3),
+  email: z.string().email(),
+  age: z.number().min(18),
+});
+const app = express();
