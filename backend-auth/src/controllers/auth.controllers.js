60fdf059c09503e6099a8217a49f2797853a1520
Suraj Thapa <surajthapa7787@gmail.com>
2026-07-20 11:22:32 +0530

    register api upgrade duplication not allowed

--- backend-auth/src/controllers/auth.controllers.js
+++ backend-auth/src/controllers/auth.controllers.js
@@ -7,6 +7,16 @@ dotenv.config();
 export async function registerUser(req, res) {
   const { username, email, password } = req.body;
 
+  const isUserAlreadyExists = await userModel.findOne({
+    email,
+  });
+
+  if (isUserAlreadyExists) {
+    return res.status(409).json({
+      message: "user already exist",
+    });
+  }
+
   const user = await userModel.create({
     username,
     email,
--- backend-auth/src/models/user.model.js
+++ backend-auth/src/models/user.model.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 const userSchema = mongoose.Schema({
   username: String,
-  email: String,
+  email: {
+    type: String,
+    unique: true,
+  },
   password: String,
 });
 
